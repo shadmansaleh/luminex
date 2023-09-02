b799c4ed161f3f4a069faaadd059f3d35ee1c5af
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2023-09-02 19:42:55 +0600

    add boilerplate

---
+++ src/main/java/com/oi/luminex/Luminex.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.oi.luminex;
+
+/**
+ *
+ * @author shadman saleh
+ * @author raihanul hasan
+ */
+public class Luminex {
+
+    public static void main(String[] args) {
+
+    }
+}
