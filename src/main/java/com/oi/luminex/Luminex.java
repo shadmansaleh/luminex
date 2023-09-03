8fe0628761acad6fc1bfb1257998ca220d212c6f
shadmansaleh <13149513+shadmansaleh@users.noreply.github.com>
2023-09-04 00:42:38 +0600

    feat: create window

--- src/main/java/com/oi/luminex/Luminex.java
+++ src/main/java/com/oi/luminex/Luminex.java
@@ -4,14 +4,39 @@
 
 package com.oi.luminex;
 
+import javax.swing.*;
+import java.awt.event.*;
+import com.oi.luminex.pages.*;
+
 /**
  *
  * @author shadman saleh
  * @author raihanul hasan
  */
-public class Luminex {
+public class Luminex extends JFrame {
 
-    public static void main(String[] args) {
-        System.out.println("hell");
-    }
+  int WINWIDTH = 1280, WINHEIGHT = 800;
+  JPanel cur_page = null;
+
+  public static void main(String[] args) {
+    Luminex lm = new Luminex();
+  }
+
+  public Luminex() {
+    setSize(WINWIDTH, WINHEIGHT);
+    setLayout(null);
+    setResizable(false);
+    setTitle("Luminex");
+    ImageIcon icon = new ImageIcon("res/images/icon/icon.jpeg");
+    setIconImage(icon.getImage());
+    setDefaultCloseOperation(EXIT_ON_CLOSE);
+    setVisible(true);
+  }
+
+  public void load_page(JPanel jp) {
+    if (cur_page != null) remove(cur_page);
+    jp.setBounds(0, 0, WINWIDTH, WINHEIGHT);
+    add(jp);
+    cur_page = jp;
+  }
 }
